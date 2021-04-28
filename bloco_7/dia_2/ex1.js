acb80b15327d208355757f7b4575436c35be2e0f
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-28 15:16:46 -0300

    organizando pastas

---
+++ bloco_7/dia_2/ex1.js
@@ -0,0 +1,56 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        margherita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+const customerInfo = (order) => {
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const customerName = order['name'];
+  const customerPhone = order['phoneNumber'];
+  const customerStreet = order.address['street'];
+  const numberStreet = order.address['number'];
+  const numberApt = order.address['apartment'];
+  
+  
+  console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, ${customerStreet} Nº: ${numberStreet}, AP: ${numberApt}`);
+}
+  
+customerInfo(order);
+  
+const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+}
+  
+orderModifier(order);
